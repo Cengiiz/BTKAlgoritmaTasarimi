3336a6bf2f34bf35c5757fe16e79a1c76764662f
Cengiiz <dadem64@gmail.com>
2023-01-30 00:35:02 +0300

    Sayinin rakamlarinin toplamini bulmak

---
+++ Egitim Icerigi/Egitim Icerigi/RakamlariTopla.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egitim_Icerigi
+{
+    internal class RakamlariTopla
+    {
+        public static int Islem(int n)
+        {
+            int toplam = 0,rakam=0;
+            while (n>0)
+            {
+                rakam = n % 10;
+                toplam += rakam;
+                n/= 10;
+            }
+
+            return toplam;
+        }
+        public static void Fonk()
+        {
+            int x = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("{0}", Islem(x));
+            Console.Read();
+        }
+    }
+}
