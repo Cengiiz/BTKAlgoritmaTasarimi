37a437b5b016af25fd259d5abab6d3672126dd35
Cengiiz <dadem64@gmail.com>
2023-01-29 17:10:01 +0300

    Byte,int16,int32 min max degerleri

--- Değişken Uygulaması/Değişken Uygulaması/Program.cs
+++ Değişken Uygulaması/Değişken Uygulaması/Program.cs
@@ -10,6 +10,24 @@ namespace Değişken_Uygulaması
     {
         static void Main(string[] args)
         {
+            int max, min;
+            max = System.Int16.MaxValue;
+            min=System.Int16.MinValue;
+
+            int max32 = System.Int32.MaxValue;
+            int min32 = System.Int32.MinValue;
+
+            byte minbyte=System.Byte.MinValue;
+            byte maxbyte=System.Byte.MaxValue;
+
+            int sayi = 23;
+            sayi = sayi * 2;
+            //Console.WriteLine(sayi);
+            Console.WriteLine("Byte -> Min:{0} \t Max:{1}", minbyte, maxbyte);
+            Console.WriteLine("Int16 -> Min:{0} \t Max:{1}",min,max);
+            Console.WriteLine("Int32 -> Min:{0} \t Max:{1}", min32, max32);
+            Console.Read();
+
         }
     }
 }
