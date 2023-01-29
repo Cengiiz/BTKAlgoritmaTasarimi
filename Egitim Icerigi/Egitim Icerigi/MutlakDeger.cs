bd08d74ba6c9f6fef3c1f8170391a7f4a0983c24
Cengiiz <dadem64@gmail.com>
2023-01-30 00:12:32 +0300

    Mutlak Deger

---
+++ Egitim Icerigi/Egitim Icerigi/MutlakDeger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egitim_Icerigi
+{
+    internal class MutlakDeger
+    {
+        public static int MutlakIslem(int n)
+        {
+            return n > 0 ? n : n * -1;
+        }
+        public static void Fonk()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (TPD1Sayi.Tekmi(n))
+                Console.WriteLine("|{0}|={1} ", n,MutlakIslem(n));
+            Console.Read();
+        }
+    }
+}
