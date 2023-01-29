84f9d7081c1c5741d48fa9089885a88d2fa53b5d
Cengiiz <dadem64@gmail.com>
2023-01-29 18:42:57 +0300

    if else uygulamasi 1

---
+++ Egitim Icerigi/Egitim Icerigi/IfElseYapisi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egitim_Icerigi
+{
+    internal class IfElseYapisi
+    {
+        public static void Fonk()
+        {
+            Console.WriteLine("-----besinci ders uygulamasi-----");
+            Console.WriteLine("Bir karakter giriniz:");
+            char ch = (char)Console.Read();
+            if (Char.IsUpper(ch))
+            {
+                Console.WriteLine("Girilen karakter buyuk bir karakterdir.");
+            }
+            else if(Char.IsLower(ch))
+            {
+                Console.WriteLine("Girilen karakter kucuk bir karakterdir.");
+
+            }
+            else  if(Char.IsDigit(ch))
+            {
+                Console.WriteLine("Girilen karakter sayidir.");
+            }
+            else
+            {
+                Console.WriteLine("Karakter alfanumerik bir ifade degildir");
+            }
+            Console.Read();
+        }
+    }
+}
