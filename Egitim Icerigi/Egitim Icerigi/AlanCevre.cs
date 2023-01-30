34e591a032a133c488d50600b90efbe715dabdd7
Cengiiz <dadem64@gmail.com>
2023-01-30 13:39:53 +0300

    Dairenin alanini ve cevresini hesaplama

---
+++ Egitim Icerigi/Egitim Icerigi/AlanCevre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egitim_Icerigi
+{
+    internal class Cember
+    {
+        public const double pi = 3.14;
+        public static double Cevresi(double r)
+        {
+            return 2*pi*r;
+        }
+        public static void Fonk()
+        {
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Cevresi(r));
+        }
+    }
+}
