30d2e3225e6b284630a0cb7ba36d376f6a0cbed7
Cengiiz <dadem64@gmail.com>
2023-01-30 15:58:27 +0300

    class ismi degisti(Cember=>AlanCevre),fonksiyonlar olusturuldu

--- Egitim Icerigi/Egitim Icerigi/AlanCevre.cs
+++ Egitim Icerigi/Egitim Icerigi/AlanCevre.cs
@@ -6,17 +6,42 @@ using System.Threading.Tasks;
 
 namespace Egitim_Icerigi
 {
-    internal class Cember
+    internal class AlanCevre
     {
         public const double pi = 3.14;
+        /// <summary>
+        /// Cevre hesaplar
+        /// </summary>
+        /// <param name="r">Yaricap</param>
+        /// <returns>Cevre degeridir</returns>
         public static double Cevresi(double r)
         {
             return 2*pi*r;
+        }
+        /// <summary>
+        /// Alan hesaplar
+        /// </summary>
+        /// <param name="r">Yaricap</param>
+        /// <returns>Alan degeridir</returns>
+        public static double Alan(double r)
+        {
+            return pi*r*r;
+        }
+        /// <summary>
+        /// Alanini hesaplar
+        /// </summary>
+        /// <param name="r">Yaricap</param>
+        /// <param name="aci">Gordugu aci</param>
+        /// <returns>Alan degeridir</returns>
+        public static double Alan(double r,int aci)
+        {
+            return pi * r * r*aci/360;
         }
         public static void Fonk()
         {
             double r = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine(Cevresi(r));
         }
+
     }
 }
