cd9d1def1ec267330093d762f44bb151826d466b
Cengiiz <dadem64@gmail.com>
2023-01-31 00:56:15 +0300

    Matematik islemleri ust alma fonksiyonu olusturuldu

---
+++ Egitim Icerigi/Egitim Icerigi/MatematikIslemleri.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egitim_Icerigi
+{
+    internal class MatematikIslemleri
+    {
+        public static double UstAlma(double taban,double kuvvet)
+        {
+            double s = 1;
+            for (int i = 0; i < kuvvet; i++)
+                s *= taban;
+            return s;
+        }
+    }
+}
