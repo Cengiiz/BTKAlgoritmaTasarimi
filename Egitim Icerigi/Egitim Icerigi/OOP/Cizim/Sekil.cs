6225aea55b896c827b637e3a41146667d6eac5c5
Cengiiz <dadem64@gmail.com>
2023-02-03 19:16:03 +0300

    oop 3

---
+++ Egitim Icerigi/Egitim Icerigi/OOP/Cizim/Sekil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egitim_Icerigi.OOP.Cizim
+{
+    public class Pozisyon
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+    public class Boyut
+    {
+        public int Genislik { get; set; }
+        public int Yukseklik { get; set; }
+    }
+    public class Sekil
+    {
+        public Pozisyon pozisyon { get; set; } = new Pozisyon();
+        public Boyut boyut { get; set; }=new Boyut();
+        public virtual void Ciz() => Console.WriteLine($"Sekil{pozisyon}-{boyut}");
+    }
+}
